7dcb061905ad24704f61386495d8179f0da95bd2
Andrew Burford <andrewsburford@gmail.com>
2022-11-27 22:41:01 -0500

    work with dorothea sparse matrices

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import numpy as np
 # https://github.com/jundongl/scikit-feature
 from skfeature.function.information_theoretical_based import MIM
 from skfeature.function.statistical_based import gini_index
+from scipy.sparse import coo_array
 
 def plot_num(xi, name='plot'):
 	plt.figure()
@@ -31,6 +32,21 @@ def get_gisette_data(dtype='train'):
 		y = np.array(y).squeeze()
 	return x, y
 
+def get_dorothea_data(dtype='train'):
+	with open('dorothea/dorothea_%s.data' % dtype) as f:
+		r = []
+		c = []
+		for i, line in enumerate(f.readlines()):
+			new_c = [int(x) - 1 for x in line.split()]
+			r.extend([i] * len(new_c))
+			c.extend(new_c)
+		d = [1] * len(r)
+		x = coo_array((d, (r, c)), shape=(i + 1, 100000))
+	with open('dorothea/dorothea_%s.labels' % dtype) as f:
+		y = [[int(y) for y in line.split()] for line in f.readlines()]
+		y = np.array(y).squeeze()
+	return x, y
+
 def add_noise(X, strength):
 	batch_size, input_size = X.shape
 	cov = strength * np.identity(input_size)
@@ -40,16 +56,20 @@ def add_noise(X, strength):
 def get_gradient(X, y, theta, l1_norm=False):
 	np.set_printoptions(threshold=np.inf)
 	m, n = X.shape
-	y = np.resize(y, (n, m))
-	z = X * y.transpose()
-	grad = z @ theta
+	if isinstance(X, coo_array):
+		z = X.copy()
+		to_negate = np.isin(z.row, np.where(y == -1))
+		z.data[to_negate] = z.data[to_negate] * -1
+	else:
+		y = np.resize(y, (n, m))
+		z = X * y.transpose()
+	grad = z @ theta # (m, 1)
 	grad = 1 / (1 + np.exp(-grad))
 	grad = 1 - grad
-	grad = np.resize(grad, (n, m))
-	grad = grad.transpose() * z
+	grad = z.transpose() @ grad
+	grad /= m
 	if l1_norm:
 		grad += np.sign(theta)
-	grad = grad.mean(axis=0)
 	return -grad
 
 def calc_accuracy(X, y, theta):
@@ -57,12 +77,13 @@ def calc_accuracy(X, y, theta):
 	y_hat = np.sign(X @ theta)
 	return sum(y_hat == y.squeeze()) / m
 
-def gd(X, y, iters=500, alpha=0.01):
+def gd(X, y, iters=500, alpha=1, l1_norm=False):
 	m, n = X.shape
 	theta = np.random.normal(size=n)
 	for i in range(iters):
 		print('\r%.2f%%' % (i / iters), end='')
-		theta -= alpha * get_gradient(X, y, theta)
+		theta -= alpha * get_gradient(X, y, theta, l1_norm)
+	print()
 	return theta
 
 def sgd(X, y, batch_size, iters=500, alpha=0.01, l1_norm=False):
@@ -71,93 +92,136 @@ def sgd(X, y, batch_size, iters=500, alpha=0.01, l1_norm=False):
 	window = 0
 	for i in range(iters):
 		if i % 10:
-			print('\r%.2f%%' % (i / iters), end='')
-		bx, by = X[window:window + batch_size], y[window:window + batch_size]
-		if window + batch_size > m:
-			bx = np.concatenate((bx, X[:window + batch_size - m]))
-			by = np.concatenate((by, Y[:window + batch_size - m]))
-		theta -= get_gradient(bx, by, theta, l1_norm)
+			pass
+			#print('\r%.2f%%' % (i / iters), end='')
+		if isinstance(X, coo_array):
+			by = y[window:window + batch_size]
+			select = (window <= X.row) & (X.row < window + batch_size)
+			bxd, bxr, bxc = X.data[select], X.row[select], X.col[select]
+			if window + batch_size > m:
+				select = X.row < (window + batch_size - m)
+				bxd = np.concatenate((bxd, X.data[select]))
+				bxr = np.concatenate((bxr, X.row[select]))
+				bxc = np.concatenate((bxc, X.col[select]))
+				by = np.concatenate((by, y[:window + batch_size - m]))
+			# add explicit 0 to ensure full length feature vector
+			bxd = np.concatenate((bxd, np.array([0])))
+			bxr = np.concatenate((bxr, np.array([bxr[-1]])))
+			bxc = np.concatenate((bxc, np.array([n-1])))
+			bx = coo_array((bxd, (bxr, bxc)))
+		else:
+			bx, by = X[window:window + batch_size], y[window:window + batch_size]
+			if window + batch_size > m:
+				bx = np.concatenate((bx, X[:window + batch_size - m]))
+				by = np.concatenate((by, y[:window + batch_size - m]))
+		theta -= alpha * get_gradient(bx, by, theta, l1_norm)
 		window += batch_size
 		window %= m
 	print()
 	return theta
+
+def select_feats(X, feats):
+	if isinstance(X, coo_array):
+		select = np.isin(X.col, feats)
+		new_shape = (X.shape[0], len(feats))
+		cols = X.col[select]
+		for i, f in enumerate(feats):
+			cols[cols == f] = i
+		return coo_array((X.data[select], (X.row[select], cols)), shape=new_shape)
+	else:
+		return X.T[feats].T
 
 def run_oneshot(X, y, small_n, iters=100, print_progress=True, train_delay=0):
 	accs = []
 	for i in range(iters):
 		theta = np.random.normal(size=n)
 		if train_delay:
-			theta = sgd(X, y, batch_size, iters=train_delay, l1_norm=True)
+			theta = gd(X, y, iters=train_delay, l1_norm=True)
 		grad = abs(get_gradient(X, y, theta))
 		if print_progress:
 			print('\r%.2f%%' % (i / iters), end='')
 		biggest = grad.argsort()
-		Xsub = X.T[biggest < small_n].T
-		theta = sgd(Xsub, y, batch_size)
-		Xvsub = Xv.T[biggest < small_n].T
+		Xsub = select_feats(X, np.where(biggest < small_n)[0])
+		#theta = sgd(Xsub, y, batch_size, iters=500, alpha=0.01)
+		theta = gd(Xsub, y)
+		Xvsub = select_feats(Xv, np.where(biggest < small_n)[0])
 		acc = 100 * calc_accuracy(Xvsub, yv, theta)
 		accs.append(acc)
 	if print_progress:
 		print()
 	return accs
 
-def run_mim(X, y, small_n, cache='gisette-mim'):
+def run_mim(X, y, small_n, cache='dorothea-mim'):
 	fn = '%s.txt' % cache
 	if os.path.isfile(fn):
 		feats = np.loadtxt(fn, dtype='int32')
 	else:
 		# note that I modified this mim function because by default it is O(n**3)
-		# but should be O(n**2)
+		# but should be O(nlogn)
 		feats, _, _ = MIM.mim(X, y)
 		np.savetxt(fn, feats)
 	feats = feats[:small_n]
-	Xsub = X.T[feats].T
-	theta = sgd(Xsub, y, batch_size)
-	Xvsub = Xv.T[feats].T
+	Xsub = select_feats(X, feats)
+	theta = gd(Xsub, y)
+	if isinstance(Xv, coo_array):
+		select = np.isin(Xv.col, feats)
+		new_shape = (Xv.shape[0], small_n)
+		cols = Xv.col[select]
+		for i, f in enumerate(feats):
+			cols[cols == f] = i
+		Xvsub = coo_array((Xv.data[select], (Xv.row[select], cols)), shape=new_shape)
+	else:
+		Xvsub = Xv.T[feats].T
 	acc = 100 * calc_accuracy(Xvsub, yv, theta)
 	return acc
 
-def run_gini(X, y, small_n, cache='gisette-gini'):
+def run_gini(X, y, small_n, cache='dorothea-gini'):
 	fn = '%s.txt' % cache
 	if os.path.isfile(fn):
 		gini_rank = np.loadtxt(fn)
 	else:
 		gini_rank = gini_index.gini_index(X, y).argsort()
 		np.savetxt(fn, gini_rank)
-	Xsub = X.T[gini_rank < small_n].T
-	theta = sgd(Xsub, y, batch_size)
-	Xvsub = Xv.T[gini_rank < small_n].T
+	Xsub = select_feats(X, np.where(gini_rank < small_n)[0])
+	theta = gd(Xsub, y)
+	Xvsub = select_feats(Xv, np.where(gini_rank < small_n)[0])
 	acc = 100 * calc_accuracy(Xvsub, yv, theta)
 	return acc
 
-def get_histogram_data():
-	accs = run_oneshot()
+def get_histogram_data(small_n, dataset_name):
+	accs = run_oneshot(X, y, small_n, iters=100)
 	accs = np.array(accs)
 	np.savetxt('accs.txt', accs)
 	print('calculating mim')
-	acc_mim = run_mim(X, y, small_n)
+	acc_mim = run_mim(X, y, small_n, cache='%s-mim' % dataset_name)
 	print(acc_mim)
 	print('calculating gini')
-	acc_gini = run_gini(X, y, small_n)
+	acc_gini = run_gini(X, y, small_n, cache='%s-gini' % dataset_name)
 	print(acc_gini)
 
 def make_histogram():
 	# replace with actual values
-	mim = 93.8
-	gini = 93.3
+	# gisette
+	mim = 95.1
+	gini = 92.7
 	accs = np.loadtxt('accs.txt')
+	# dorothea
+	mim = 20.57
+	gini = 84.29
+	accs = np.loadtxt('dorothea-accs.txt')
 
-	plt.hist(accs, bins=20, label='Atomic One-Shot')
+	plt.hist(accs, bins=10, label='Atomic One-Shot')
 	plt.xlabel('Test Accuracy (%)')
 	plt.ylabel('Count')
-	plt.title('Test Accuracy On Gisette With 300 Features')
+	plt.title('Repeated Trials of Atomic One-Shot (n=1024)')
 	plt.axvline(mim, color='r', label='MIM')
 	plt.axvline(gini, color='y', label='Gini Index')
 	plt.legend()
-	plt.xlim(55, 100)
-	plt.savefig('hist.pdf')
+	plt.xlim(0, 100)
+	plt.savefig('dorothea-hist.pdf')
 
-small_ns = [2**i for i in list(range(3, 12))] + [5000]
+small_ns = [2**i for i in list(range(3, 17))] + [100000]
+#small_ns = [2**i for i in list(range(3, 10))]
 
 def get_linegraph_data():
 	# oneshot, mim, gini
@@ -173,17 +237,17 @@ def get_linegraph_data():
 		t1 = time()
 		accs.append((accs_oneshot.mean(), acc_mim, acc_gini))
 	accs = np.array(accs)
-	np.savetxt('linegraph-accs.txt', accs)
+	np.savetxt('dorothea-linegraph-accs.txt', accs)
 
 def make_linegraph():
-	accs = np.loadtxt('linegraph-accs.txt')
-	print(len(small_ns))
-	trunc = 10
+	accs = np.loadtxt('dorothea-linegraph-accs.txt')
+	trunc = len(small_ns)
 	plt.plot(small_ns[:trunc], accs[:,0][:trunc], label='Atomic One-Shot')
 	plt.plot(small_ns[:trunc], accs[:,1][:trunc], label='MIM')
 	plt.plot(small_ns[:trunc], accs[:,2][:trunc], label='Gini')
 	plt.semilogx()
-	plt.xticks(ticks=small_ns, labels=small_ns)
+	labels = ['$2^{%d}$' % i for i in range(3, 17)] + ['$10^5$']
+	plt.xticks(ticks=small_ns, labels=labels)
 	plt.minorticks_off()
 	plt.legend()
 	plt.ylabel('Test Accuracy (%)')
@@ -198,13 +262,13 @@ def get_boxplot_data():
 	accs = np.zeros((num_samples, iters))
 	for i, train_delay in enumerate(delays):
 		print('train_delay', train_delay)
-		accs[i,:] = run_oneshot(X, y, 256, iters=iters, train_delay=train_delay)
-	np.savetxt('boxplot.txt', accs)
+		accs[i,:] = run_oneshot(X, y, 1024, iters=iters, train_delay=train_delay)
+	np.savetxt('dorothea-boxplot.txt', accs)
 
 def make_boxplot():
 	num_samples = 10
 	delays = [0] + [2**i for i in range(num_samples-1)]
-	accs = np.loadtxt('boxplot.txt')
+	accs = np.loadtxt('dorothea-boxplot.txt')
 	for i, delay in enumerate(delays):
 		if not delay:
 			delay = 0.5
@@ -213,17 +277,17 @@ def make_boxplot():
 	plt.xlabel('Training Delay')
 	plt.ylim((55, 100))
 	plt.ylabel('Test Accuracy (%)')
-	plt.title('Test Accuracy As Training Delay Increases')
+	plt.title('Test Accuracy As Training Delay Increases (n=1024)')
 	plt.xticks(ticks=[0.5] + delays[1:], labels=[0] + delays[1:])
 	plt.minorticks_off()
 	#plt.legend()
-	plt.savefig('boxplot.pdf')
+	plt.savefig('dorothea-boxplot.pdf')
 	
 	
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--feature_size', default=300)
+	parser.add_argument('--feature_size', default=256)
 	parser.add_argument('--batch_size', default=300)
 
 	args = parser.parse_args()
@@ -239,9 +303,23 @@ if __name__ == '__main__':
 	#m, n = X.shape
 	#print('loaded %d samples' % m)
 
-	#compute_300_vals()
+	print('loading dorothea data')
+	X, y = get_dorothea_data()
+	#X = X.toarray()
+	m, n = X.shape
+	if isinstance(X, coo_array):
+		z = X.copy()
+		to_negate = np.isin(z.row, np.where(y == -1))
+		z.data[to_negate] = z.data[to_negate] * -1
+	else:
+		z = X * np.resize(y, (n, m)).transpose()
+	Xv, yv = get_dorothea_data('valid')
+	m, n = X.shape
+	print('loaded %d samples' % m)
+
+	#get_histogram_data(256, 'gisette')
 	#make_histogram()
 	#get_linegraph_data()
-	#make_linegraph()
+	make_linegraph()
 	#get_boxplot_data()
-	make_boxplot()
+	#make_boxplot()
